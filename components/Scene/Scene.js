e49f91226a2cce5e7ab2aedb373e4fe9498c6d60
thomas-rooty <essainono1@gmail.com>
2022-05-13 11:49:41 +0200

    basic tps movement

--- components/Scene/Scene.js
+++ components/Scene/Scene.js
@@ -1,21 +1,44 @@
-import {Canvas} from '@react-three/fiber';
-import {useEffect, useState} from 'react';
+import {Canvas, extend, useFrame, useThree} from '@react-three/fiber';
+import {useEffect, useRef, useState} from 'react';
+import {degrees} from "three/examples/jsm/nodes/shadernode/ShaderNodeBaseElements";
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {degToRad} from "three/src/math/MathUtils";
 
 const Scene = () => {
+    const [meshPosition, setMeshPosition] = useState([0, 0, 0]);
+
+    extend({OrbitControls});
+    const Controls = () => {
+        const {camera, gl} = useThree();
+        const controls = useRef();
+        camera.position.set(meshPosition[0], meshPosition[1], meshPosition[2]-5);
+        useFrame(() => controls.current.update());
+        return <orbitControls
+            ref={controls}
+            args={[camera, gl.domElement]}
+            enableZoom={false}
+            enableDamping={true}
+            enablePan={false}
+            enableRotate={false}
+            target={meshPosition}
+            enabled={true}
+        />;
+    }
+
     const changePosition = (event) => {
         // Handle cases where keys Z, Q, S, D are pressed individually
         switch (event.key) {
             case 'z':
-                setMeshPosition([meshPosition[0], meshPosition[1], meshPosition[2] - 0.1]);
-                break;
-            case 'q':
-                setMeshPosition([meshPosition[0] - 0.1, meshPosition[1], meshPosition[2]]);
-                break;
-            case 's':
                 setMeshPosition([meshPosition[0], meshPosition[1], meshPosition[2] + 0.1]);
                 break;
-            case 'd':
+            case 'q':
                 setMeshPosition([meshPosition[0] + 0.1, meshPosition[1], meshPosition[2]]);
+                break;
+            case 's':
+                setMeshPosition([meshPosition[0], meshPosition[1], meshPosition[2] - 0.1]);
+                break;
+            case 'd':
+                setMeshPosition([meshPosition[0] - 0.1, meshPosition[1], meshPosition[2]]);
                 break;
             default:
                 break;
@@ -29,22 +52,33 @@ const Scene = () => {
         }
     }, [changePosition]);
 
-    const [meshPosition, setMeshPosition] = useState([0, 0, 0]);
-
-
     return (
         <Canvas>
+            <Controls/>
             <ambientLight intensity={0.5}/>
             <pointLight position={[2, 2, 2]}/>
             <mesh
                 position={meshPosition}
-                rotation={[0, 0, 0]}
+                rotation={[-35, -20, 0]}
                 scale={[1, 1, 1]}
             >
                 <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
                 <meshStandardMaterial
                     attach="material"
                     color={0xff55ff}
+                    roughness={0.5}
+                    metalness={0.5}
+                />
+            </mesh>
+            <mesh
+                position={[0, 0, 0]}
+                rotation={[0, 0, 0]}
+                scale={[1, 1, 1]}
+            >
+                <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
+                <meshStandardMaterial
+                    attach="material"
+                    color={0x55ffff}
                     roughness={0.5}
                     metalness={0.5}
                 />
