bf3d895500d6e0ec0c346cc5e4e9848e17a5469a
thomas-rooty <essainono1@gmail.com>
2022-05-13 10:41:38 +0200

    moving a cube with zqsd

--- components/Scene/Scene.js
+++ components/Scene/Scene.js
@@ -1,13 +1,44 @@
 import {Canvas} from '@react-three/fiber';
+import {useEffect, useState} from 'react';
 
 const Scene = () => {
+    const changePosition = (event) => {
+        // Handle cases where keys Z, Q, S, D are pressed individually
+        switch (event.key) {
+            case 'z':
+                setMeshPosition([meshPosition[0], meshPosition[1], meshPosition[2] - 0.1]);
+                break;
+            case 'q':
+                setMeshPosition([meshPosition[0] - 0.1, meshPosition[1], meshPosition[2]]);
+                break;
+            case 's':
+                setMeshPosition([meshPosition[0], meshPosition[1], meshPosition[2] + 0.1]);
+                break;
+            case 'd':
+                setMeshPosition([meshPosition[0] + 0.1, meshPosition[1], meshPosition[2]]);
+                break;
+            default:
+                break;
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', changePosition);
+        return () => {
+            document.removeEventListener('keydown', changePosition);
+        }
+    }, [changePosition]);
+
+    const [meshPosition, setMeshPosition] = useState([0, 0, 0]);
+
+
     return (
         <Canvas>
             <ambientLight intensity={0.5}/>
             <pointLight position={[2, 2, 2]}/>
             <mesh
-                position={[0, 0, 0]}
-                rotation={[2, 0, 2]}
+                position={meshPosition}
+                rotation={[0, 0, 0]}
                 scale={[1, 1, 1]}
             >
                 <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
