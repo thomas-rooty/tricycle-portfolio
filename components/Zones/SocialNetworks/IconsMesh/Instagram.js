a2b208d8d64ab5cbaff5251e99831de602112a1d
thomas-rooty <tc.caron91@gmail.com>
2022-07-22 10:30:42 +0200

    added spline instagram logo

--- components/Zones/SocialNetworks/IconsMesh/Instagram.js
+++ components/Zones/SocialNetworks/IconsMesh/Instagram.js
@@ -1,40 +1,21 @@
 import {useBox} from "@react-three/cannon";
 import React from "react";
-import {RoundedBox} from "@react-three/drei";
+import InstagramIcon from "./InstagramIcon";
 
 // Args is scale, position is x, y, z
-
-const Instagram = ({args = [2.5, 2.5, 0.5], ...props}) => {
+const Icons = () => {
 	const [ref] = useBox(() => ({
 		type: "Static",
-		args,
-		...props,
+		args: [2, 3, 1],
+		position: [7.15, 1.25, -1],
+		rotation: [0, 0, 0],
+		userData: {
+			id: "instagram",
+		}
 	}));
 	return (
-		<group ref={ref} >
-			<mesh castShadow>
-				<RoundedBox args={[2.5, 2.5, 0.5]} radius={0.3} smoothness={3}>
-					<meshPhongMaterial color="#f05444"/>
-				</RoundedBox>
-			</mesh>
-			<mesh position={[0, 0, 0.3]} scale={[0.7, 0.7, 1]}>
-				<ringBufferGeometry args={[0.7, 1, 32]} />
-				<meshBasicMaterial attach="material" color="#ffffff"/>
-			</mesh>
-			<mesh position={[0.8, 0.7, 0.3]} scale={[0.15, 0.15, 1]}>
-				<ringBufferGeometry args={[0, 1, 32]} />
-				<meshBasicMaterial attach="material" color="#ffffff"/>
-			</mesh>
-		</group>
-	);
-};
-
-const Icons = () => {
-	return (
-		<group>
-			<Instagram
-				position={[7, 1.5, -1]}
-				rotation={[0, 0, 0]}
+		<group ref={ref}>
+			<InstagramIcon
 				userData={{id: "instagram"}}
 			/>
 		</group>
