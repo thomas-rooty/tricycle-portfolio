f6ddcd0110662ec1981781aacbfba4020c6843a0
thomas-rooty <essainono1@gmail.com>
2022-07-22 20:25:35 +0200

    icons now levitate

--- components/Zones/SocialNetworks/IconsMesh.js
+++ components/Zones/SocialNetworks/IconsMesh.js
@@ -4,14 +4,15 @@ import Instagram from "./Mesh/Instagram";
 import LinkedIn from "./Mesh/LinkedIn";
 import GitHub from "./Mesh/GitHub";
 import Stackoverflow from "./Mesh/Stackoverflow";
+import {useFrame} from "@react-three/fiber";
 
 // Args is scale, position is x, y, z
 const Icons = () => {
 	// Instagram physics box
-	const [instagramRef] = useBox(() => ({
+	const [instagramRef, instagramApi] = useBox(() => ({
 		type: "Static",
 		args: [2, 3, 1],
-		position: [6.15, 1.25, -1],
+		position: [6.15, 1.5, -1],
 		rotation: [0, -0.12, 0],
 		userData: {
 			id: "instagram",
@@ -19,7 +20,7 @@ const Icons = () => {
 	}));
 
 	// LinkedIn physics box
-	const [linkedinRef] = useBox(() => ({
+	const [linkedinRef, linkedinApi] = useBox(() => ({
 		type: "Static",
 		args: [2, 3, 1],
 		position: [10.5, 1.15, 0.5],
@@ -30,7 +31,7 @@ const Icons = () => {
 	}));
 
 	// GitHub physics box
-	const [githubRef] = useBox(() => ({
+	const [githubRef, githubApi] = useBox(() => ({
 		type: "Static",
 		args: [2, 3, 1],
 		position: [19.5, 1.15, 4.5],
@@ -41,7 +42,7 @@ const Icons = () => {
 	}));
 
 	// Stackoverflow physics box
-	const [stackoverflowRef] = useBox(() => ({
+	const [stackoverflowRef, stackoverflowApi] = useBox(() => ({
 		type: "Static",
 		args: [2, 3, 1],
 		position: [15.2, 0, 2.5],
@@ -50,6 +51,20 @@ const Icons = () => {
 			id: "stackoverflow",
 		}
 	}));
+
+	useFrame(({clock}) => {
+		// Make the icons go up and down
+		instagramApi.position.set(6.15, (Math.sin(clock.getElapsedTime()) * 0.5) + 2.5, -1);
+		linkedinApi.position.set(10.5, (Math.sin(clock.getElapsedTime() + 5) * 0.5) + 2.5, 0.5);
+		githubApi.position.set(19.5, (Math.sin(clock.getElapsedTime() - 2) * 0.5) + 2.5, 4.5);
+		stackoverflowApi.position.set(15.2, (Math.sin(clock.getElapsedTime()) * 0.3) + 1, 2.5);
+
+		// Make the icons rotate
+		instagramApi.rotation.set(0, Math.sin(clock.getElapsedTime()) * 0.4, 0);
+		linkedinApi.rotation.set(0, Math.sin(clock.getElapsedTime()) * 0.15, 0);
+		githubApi.rotation.set(0, Math.sin(clock.getElapsedTime()) * 0.3 - 0.3, 0);
+		stackoverflowApi.rotation.set(0, Math.sin(clock.getElapsedTime()) * 0.1, 0);
+	});
 
 	return (
 		<>
