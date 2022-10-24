8b4ae3323c52d2866d091ffc67a33c5048f68423
thomas-rooty <essainono1@gmail.com>
2022-10-24 12:01:32 +0200

    rmeoved effects

--- components/effects/effects.js
+++ components/effects/effects.js
@@ -6,14 +6,5 @@ import {
 
 export const Effects = () => {
   return (
-    <EffectComposer>
-      <DepthOfField
-        focusDistance={0}
-        focalLength={0.075}
-        bokehScale={2}
-        height={480}
-      />
-      <SSAO />
-    <EffectComposer/>
   );
 };
