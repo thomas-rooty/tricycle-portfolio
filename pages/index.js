25801cf4c1ecb00df0d53b32789720131a6cc60b
thomas-rooty <essainono1@gmail.com>
2022-11-03 11:43:21 +0100

    moved error boundary to physics scene

--- pages/index.js
+++ pages/index.js
@@ -32,65 +32,65 @@ const Home = () => {
 
   return (
     <div className={styles.appContainer}>
-      <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <Head>
-          <title>Thomas Caron - Ride to discover me!</title>
-          <meta
-            name='description'
-            content='Fullstack developer living in Versailles, France. Student at IPSSI, developer at Quietalis, freelancer.'
+      <Head>
+        <title>Thomas Caron - Ride to discover me!</title>
+        <meta
+          name='description'
+          content='Fullstack developer living in Versailles, France. Student at IPSSI, developer at Quietalis, freelancer.'
+        />
+        <meta property='og:image' content='/assets/icons/banner.png'/>
+        <meta itemProp='image' content='/assets/icons/banner.png'/>
+        <meta itemProp='title' content='Thomas Caron - 3D Portfolio'/>
+        <meta name='twitter:image' content='/assets/icons/banner.png'/>
+        <meta property='og:title' content='Thomas Caron - Ride to discover me!'/>
+        <meta
+          property='og:description'
+          content='Fullstack developer living in Versailles, France. Student at IPSSI, developer at Quietalis, freelancer.'
+        />
+        <link rel='icon' href='/favicon.ico'/>
+      </Head>
+      <Hovernotification/>
+      <Canvas dpr={[1, 1.5]} shadows>
+        <Suspense fallback={<Loader/>}>
+          <LandingPage/>
+          <fog attach='fog' args={['#325a80', 10, 50]}/>
+          <color attach='background' args={['#325a80']}/>
+          <ambientLight intensity={lightIntensity}/>
+          <spotLight
+            position={[50, 50, 50]}
+            angle={0.4}
+            intensity={0.4}
+            castShadow
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            penumbra={1}
           />
-          <meta property='og:image' content='/assets/icons/banner.png'/>
-          <meta itemProp='image' content='/assets/icons/banner.png'/>
-          <meta itemProp='title' content='Thomas Caron - 3D Portfolio'/>
-          <meta name='twitter:image' content='/assets/icons/banner.png'/>
-          <meta property='og:title' content='Thomas Caron - Ride to discover me!'/>
-          <meta
-            property='og:description'
-            content='Fullstack developer living in Versailles, France. Student at IPSSI, developer at Quietalis, freelancer.'
-          />
-          <link rel='icon' href='/favicon.ico'/>
-        </Head>
-        <Hovernotification/>
-        <Canvas dpr={[1, 1.5]} shadows>
-          <Suspense fallback={<Loader/>}>
-            <LandingPage/>
-            <fog attach='fog' args={['#325a80', 10, 50]}/>
-            <color attach='background' args={['#325a80']}/>
-            <ambientLight intensity={lightIntensity}/>
-            <spotLight
-              position={[50, 50, 50]}
-              angle={0.4}
-              intensity={0.4}
-              castShadow
-              shadow-mapSize-width={1024}
-              shadow-mapSize-height={1024}
-              penumbra={1}
+          <Physics broadphase='SAP' contactEquationRelaxation={4} friction={1e-3} allowSleep>
+            <Floor rotation={[-Math.PI / 2, 0, 0]} userData={{id: 'floor'}}/>
+            <Vehicle
+              position={[0, 3, 0]}
+              rotation={[0, -Math.PI / 4, 0]}
+              angularVelocity={[0, 10, 0]}
+              wheelRadius={0.3}
             />
-            <Physics broadphase='SAP' contactEquationRelaxation={4} friction={1e-3} allowSleep>
-              <Floor rotation={[-Math.PI / 2, 0, 0]} userData={{id: 'floor'}}/>
-              <Vehicle
-                position={[0, 3, 0]}
-                rotation={[0, -Math.PI / 4, 0]}
-                angularVelocity={[0, 10, 0]}
-                wheelRadius={0.3}
-              />
+            <ErrorBoundary fallback={<div>Something went wrong</div>}>
               <Skatepark/>
               <SocialIconsPads/>
               <Signs/>
               <RocketObject/>
               <Projects/>
-            </Physics>
-          </Suspense>
-        </Canvas>
-        <div style={{position: 'absolute', top: 30, left: 40}}>
+            </ErrorBoundary>
+          </Physics>
+        </Suspense>
+      </Canvas>
+      <div style={{position: 'absolute', top: 30, left: 40}}>
                 <pre>
                     ZQSD, WASD or ↑ ← ↓ → to drive
                     <br/>
                     space to brake, enter to interact
                     <br/>R to reset
                 </pre>
-        </div>
-      </ErrorBoundary>
+      </div>
     </div>
   );
 };
